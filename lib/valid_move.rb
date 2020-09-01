71b6f96631b8a945f8abe65a82dfc586f7c61182
Oscar Phillip Springsteen <pspringsteen160@gmail.com>
2020-09-01 20:36:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,9 +5,9 @@ def valid_move?(board, index)
     return false
   end
   if (board[index] == "" || board[index] == " " || board[index] == nil)
-    return false
-  else 
     return true
+  else 
+    return false
   end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
